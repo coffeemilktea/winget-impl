39ef35e3d741800e16fcb90c8adbf0f42c3dc44a
Scott Wu <58385792+coffeemilktea@users.noreply.github.com>
2023-02-12 19:18:29 -0800

    Update cplusplus.ps1

--- cplusplus.ps1
+++ cplusplus.ps1
@@ -1,7 +1,12 @@
+#set mode to silent for normal install without prompt
 $ProgressPreference='Silent'
+
+#Download command
 Invoke-WebRequest -Uri https://aka.ms/vs/17/release/vc_redist.x64.exe -OutFile .\vc_redist.2022.x64.exe
 Invoke-WebRequest -Uri https://download.microsoft.com/download/1/6/B/16B06F60-3B20-4FF2-B699-5E9B7962F9AE/VSU_4/vcredist_x64.exe -OutFile .\vc_redist_2012.x64.exe
 Invoke-WebRequest -Uri https://download.microsoft.com/download/2/E/6/2E61CFA4-993B-4DD4-91DA-3737CD5CD6E3/vcredist_x64.exe -OutFile .\vc_redist_2013.x64.exe
+
+#Install Command.
 Add-AppxPackage .\vc_redist_2012.x64.exe
 Add-AppxPackage .\vc_redist_2013.x64.exe
 Add-AppxPackage .\vc_redist.2022.x64.exe
